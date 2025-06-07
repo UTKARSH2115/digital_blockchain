74a23b9fd61dbcf11e25b4c1b7e883c00014e142
Utkarsh <60812577+UTKARSH2115@users.noreply.github.com>
2025-06-07 15:19:38 +0530

    project pushed

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Footer.css';
+
+const Footer = () => {
+	return (
+        <div className="parent_footer" style={{ minHeight: '10vh' }}>
+
+        </div>
+	);
+};
+
+export default Footer;
